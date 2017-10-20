ec09cb61bc30d3e69ec181a68c437369f07c8a73
Mark Helt <heltmm@gmail.com>
2017-10-20 10:27:49 -0700

    create backend api call

---
+++ js/doctor-interface.js
@@ -0,0 +1,3 @@
+import {Doctor} from './../js/doctor.js'
+
+$()
--- js/doctor.js
+++ js/doctor.js
@@ -1,1 +1,37 @@
 const apiKey = require('./../.env').apiKey;
+
+export class Doctor{
+  constructor(){
+    this.key = apiKey;
+  }
+
+  search(url){
+    let promise = new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    let url = ulr + `user_key=${this.key}`;
+    console.log(url)
+
+    request.onload = function() {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(Error(request.response));
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+    });
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      debugger;
+      }, function(error) {
+        console.log(error);
+
+    });
+
+    }
+
+
+}
