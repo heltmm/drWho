f7bb2bcd15625df36153eec3da11ac1b74d9b47b
Mark Helt <heltmm@gmail.com>
2017-10-20 19:10:17 -0700

    add search by name

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,12 +2,16 @@ import {DoctorSearch} from './../js/doctor.js';
 
 let display = function(doctors){
   $('#doctors').html('');
-  doctors.forEach(function(doctor){
-    $('#doctors').append(
-      `<div class='card bg-light mb-3'><div class='card-body'> ${doctor.title} ${doctor.first_name} ${doctor.last_name}, Accepting patients: ${doctor.accepting_patients}<br>${doctor.city}, ${doctor.state}, ${doctor.street}, zip: ${doctor.zip}<br>${doctor.specialty}<br>Website: ${doctor.website}<br>Phone: ${doctor.phone}</div></div>`
-
-    );
-  });
+  if(doctors.length > 0){
+    doctors.forEach(function(doctor){
+      $('#doctors').append(
+        `<div class='card bg-secondary mb-3'><div class='card-body'> ${doctor.title} ${doctor.first_name} ${doctor.last_name}, ${doctor.accepting_patients ? 'Currently accepting patients' : 'Currently not accepting patients'}<br>${doctor.city}, ${doctor.state}, ${doctor.street}, zip: ${doctor.zip}<br>${doctor.specialty}<br>${doctor.website ? '<a href="' + doctor.website + '">' + doctor.website + '</a><br>' : ''}Phone: ${doctor.phone}</div></div>`
+      );
+    });
+  }
+  else {
+    $('#doctors').append('<h2>No Results Returned</h2>')
+  }
 };
 
 $(document).ready(function(){
@@ -18,10 +22,11 @@ $(document).ready(function(){
     doctorSearch.search(url, display);
   });
 
-  $('#name').click(function(){
+  $('#nameSearch').click(function(){
     let name = $('#name').val();
+    debugger;
     let doctorSearch = new DoctorSearch();
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${illness}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&`;
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&`;
     doctorSearch.search(url, display);
   });
 });
